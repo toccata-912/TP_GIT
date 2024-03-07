b076dac6cf5250920249137d07e12bbcf43944a4
Derrahi Mohamed <mohamedderrahi8@gmail.com>
2024-03-07 11:32:03 +0100

    did some changes

--- src/pack/AAA_running.java
+++ src/pack/AAA_running.java
@@ -6,8 +6,7 @@ public class AAA_running {
 
 	public static void main(String[] args) {
 		System.out.println("this is my GIT TP");
-		System.out.println("I will push to github now");
-		System.out.println("removed text");
+		
 		JFrame frame = new JFrame();
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
