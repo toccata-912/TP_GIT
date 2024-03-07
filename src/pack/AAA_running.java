f1376c20245c5c3c19583d8cccb054417250b968
toccata <130076100+toccata-912@users.noreply.github.com>
2024-03-07 12:11:08 +0100

    Update AAA_running.java

--- src/pack/AAA_running.java
+++ src/pack/AAA_running.java
@@ -7,7 +7,7 @@ public class AAA_running {
 	public static void main(String[] args) {
 		System.out.println("this is my GIT TP");
 		System.out.println("I will push to github now");
-		System.out.println("removed text");
+		System.out.println("chaning this so I can pull a dif version");
 		JFrame frame = new JFrame();
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
