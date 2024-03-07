7e9efeffa5cf29469de17d6a8ceeb2dfaa17a557
Derrahi Mohamed <mohamedderrahi8@gmail.com>
2024-03-07 10:47:12 +0100

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TP_GIT {
+	requires java.desktop;
+}
---
+++ src/pack/AAA_running.java
@@ -0,0 +1,17 @@
+package pack;
+
+import javax.swing.JFrame;
+
+public class AAA_running {
+
+	public static void main(String[] args) {
+		System.out.println("this is my GIT TP");
+		System.out.println("I will push to github now");
+		
+		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
