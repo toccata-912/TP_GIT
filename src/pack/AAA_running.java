ba0671df70c39b448692db4c4ee92110a5840a88
Derrahi Mohamed <mohamedderrahi8@gmail.com>
2024-03-07 10:52:18 +0100

    removed some text

--- src/pack/AAA_running.java
+++ src/pack/AAA_running.java
@@ -7,7 +7,7 @@ public class AAA_running {
 	public static void main(String[] args) {
 		System.out.println("this is my GIT TP");
 		System.out.println("I will push to github now");
-		System.out.println("I added some text here");
+		System.out.println("removed text");
 		JFrame frame = new JFrame();
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
